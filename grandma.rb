b2ddba5f0f33066194fdeb0bd1613b82c3fc76de
Haley DeKeyser <hcd09@live.com>
2019-05-04 18:22:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return puts "I LOVE YOU TOO PUMPKIN!"
     
   elsif phrase.upcase == phrase
-    puts "NO, NOT SINCE 1938!"
+    return puts "NO, NOT SINCE 1938!"
     
   else puts "HUH?! SPEAK UP, SONNY!"
 end
